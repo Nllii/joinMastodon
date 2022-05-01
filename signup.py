2c69630d6cff02ff06f71f5bb5c4bef7879138d6
Nllii <ch1ris23@ymail.com>
2022-04-30 22:15:48 -0500

    udpated stuff...

--- signup.py
+++ signup.py
@@ -27,12 +27,15 @@ def token(client_id,client_secret,domain):
     }
 
     response = requests.post('https://'+domain+'/oauth/token', headers=headers, json=json_data).json()
-    access_token = response['access_token']
-    return access_token
+    if response['access_token'] == None:
+        return None
+    else:
+        access_token = response['access_token']
+        return access_token
     # print(response.json())
 
 
-not_allowed_domain= "mstdn.jp","troet.cafe"
+# not_allowed_domain= "mstdn.jp","troet.cafe"
 
 
 def create_accounts():
@@ -65,13 +68,11 @@ def create_accounts():
                     'client_name': 'Mastodon for iOS',
                     'scopes': 'read write follow push',
                 }
-                if domain not in not_allowed_domain:
-                    apps = requests.post('https://'+domain+'/api/v1/apps', headers=headers, json=json_data)
-                    print(apps.json())
-                    print("\n")
-                    print("\n")
-                else:
-                    print(message_)
+                apps = requests.post('https://'+domain+'/api/v1/apps', headers=headers, json=json_data)
+                print(apps.json())
+                print("\n")
+                print("\n")
+                
             except JSONDecodeError or Exception as e:
                 print("\n")
                 continue
@@ -79,6 +80,8 @@ def create_accounts():
             client_secret =  apps.json()['client_secret']
             client_id  = apps.json()['client_id']
             access_key = token(client_id,client_secret,domain)
+            if access_key == None:
+                continue
             addy,SSN,phone,phoneprefix,birthday,age,tropicalzodiac,email,username,password,website,useragent,cardtype,card,exp,CVC,company,job,height,weight,bloodtype,UPSTrackingnum,MoneyGram,WesternUnion,favcolor,car,GUID  = fakeuser.FakeNameGenerator().GenerateIdenity()
             account_headers = {
                 
@@ -99,8 +102,7 @@ def create_accounts():
             response = requests.post('https://'+domain+'/api/v1/accounts', headers=account_headers, json=account_data)
             # save the response in a csv file
             with open('accounts.csv', 'a') as f:
-                f.write('email:'+username+temp_email_domain,'\tpassword:'+password,'\tdomain:'+domain,'username:'+username,'\n')
-
+                f.write(username+temp_email_domain+','+username+','+password+','+domain+'\n')
             print(response.json())
 
 create_accounts()
